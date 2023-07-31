7ad5aeb46eb1335c7da2a71ebc57ee00f8666417
Ezequiel <ezequiel.lesca@gmail.com>
2023-07-31 18:01:54 -0300

    About terminado

--- client/src/views/About/About.jsx
+++ client/src/views/About/About.jsx
@@ -1,7 +1,36 @@
 const About = () => {
     return(
         <div>
-            Soy el About
+              <div >
+        <img src="\assets\eze perfil.jpg" alt="Eze Lescano" />
+      </div>
+      <div >
+        <h2>DESCRIPCION</h2>
+        <br />
+        <br />
+        <div >
+          <p>
+            <b>Soy Ezequiel </b>
+            Durante 11 años trabajé en el sector de transporte público de
+            pasajeros, pero sentía que necesitaba un cambio en mi vida. Decidí
+            seguir mi pasión por la tecnología y convertirme en un Full Stack
+            Developer. Ahora, tengo sólidos conocimientos en HTML, CSS, React,
+            Redux, JavaScript, Sequelize, PostgreSQL, Express, y otras
+            tecnologías, lo que me permite diseñar y desarrollar aplicaciones
+            web de alta calidad. Estoy emocionado por seguir aprendiendo y
+            mejorando mis habilidades para continuar creciendo como profesional
+            en esta carrera que me apasiona.
+          </p>
+        </div>
+        <div >
+          <a href="https://github.com/ezelescano" target="_blank">
+            <img src="\assets\github.ico" alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/eze-lescano83/" target="_blank">
+            <img src="\assets\linkedin.ico" alt="" />
+          </a>
+        </div>
+      </div>
         </div>
     )
 }
